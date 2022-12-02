7bb743268b3415d24a3a8e50939720ef571ffba6
aarond707 <aarond707@gmail.com>
2022-12-02 00:52:43 -0800

    moved recent work into rollback branch

---
+++ scenes/characters/character00.gd
@@ -0,0 +1,8 @@
+extends Spatial
+##########################
+## THIS NODE IS A FOR ANY NPC
+## MODULATE COLOR AND MESH DEFORMS
+##########################
+
+# export var character color. probably want it to be a hex value
+# I may not even want to export it, though. it could pull from a database entry, if I connect it to that.
