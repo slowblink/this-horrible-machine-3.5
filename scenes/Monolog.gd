0a4acdbb603d4ac33c5651e6d93d9fa44c11dad8
aarond707 <aarond707@gmail.com>
2023-01-23 18:33:07 -0800

    new blockout textures

---
+++ scenes/Monolog.gd
@@ -0,0 +1,9 @@
+extends Spatial
+
+
+### Automatic References Start ###
+onready var _audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D
+### Automatic References Stop ###
+
+func _ready():
+	pass 
